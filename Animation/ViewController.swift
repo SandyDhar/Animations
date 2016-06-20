149b9f52dfdaf07755893a4a12ac6307ee1abc47
SandyDhar <sudeepthiosdev@gmail.com>
2016-06-19 21:08:51 -0400

    initial commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Sudeepth Dharavasthu on 4/12/16.
+//  Copyright © 2016 Sudeepth Dharavasthu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var counter = 1
+    @IBOutlet weak var imageViewer: UIImageView!
+    
+    @IBAction func changeImage(sender: AnyObject) {
+      
+        if counter == 5 {
+            counter = 1
+        }
+        else{
+            counter += 1
+            }
+        imageViewer.image = UIImage(named: "frame\(counter).png")
+        print("frame\(counter).png")
+        
+        }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
